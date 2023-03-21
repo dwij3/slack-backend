8e7ac1de34f5aab835901c4a151a134a86e5bde0
Dwij Bhanderi (Bhanderi) <bhanderidwij.sanjaykumar@bhanderidwij.sanjaykumar-RDM0695CXR>
2023-03-21 11:27:32 +0530

    update id

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const {v4 : uuidv4} = require('uuid')
+
 const app = express();
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+
 const fs = require("fs");
-const { json } = require("stream/consumers");
+require("dotenv").config();
 const cors = require("cors");
 app.use(
   cors({
@@ -10,63 +17,63 @@ app.use(
   })
 );
 
-
-
 //fetch currentUser
 app.get("/getCurrentUserInfo/:id", (req, res) => {
-  const data = fs.readFileSync("./database/users.json", {
+  const file = fs.readFileSync("./database/users.json", {
     encoding: "utf8",
     flag: "r",
   });
-  const userData = JSON.parse(data);
+  const userData = JSON.parse(file);
 
   const currentUser = userData.find((ele) => {
-    return ele.id == req.params.id;
+    return ele.id === req.params.id;
   });
 
-  res.send(currentUser);
+  if (currentUser) res.send(currentUser);
+  // res.send(new Error("User not found"));
 });
 
 //fetch details for teamMates
 app.get("/getUserTeamMates/:id", (req, res) => {
-  const data = fs.readFileSync("./database/users.json", {
+  const file = fs.readFileSync("./database/users.json", {
     encoding: "utf8",
     flag: "r",
   });
-  const userData = JSON.parse(data);
+  const userData = JSON.parse(file);
 
   const user = userData.find((user) => {
-    return user.id == req.params.id;
+    return user.id === req.params.id;
   });
 
   const teamMates = userData.filter((u) => {
     return user["chatRoomId"].includes(u.id);
   });
 
-  res.send(teamMates);
+  if (teamMates) res.send(teamMates);
+  res.send(new Error("teamMates not found"));
 });
 
 //fetch details for chatroom
 app.get("/getChatRoom/:userId/:teamMateId", (req, res) => {
-  const data = fs.readFileSync("./database/chatroom.json", {
+  const file = fs.readFileSync("./database/chatroom.json", {
     encoding: "utf8",
     flag: "r",
   });
-  const chatRoomData = JSON.parse(data);
-  const arr1 = [+req.params.userId, +req.params.teamMateId];
+  const chatRoomData = JSON.parse(file);
+  const reqParamas = [req.params.userId, req.params.teamMateId];
 
   //finding the chatroom based on user and teamMate
   const chatRoom = chatRoomData.find((room) => {
-    const arr2 = room["userIds"];
+    const userIds = room["userIds"];
 
-    arr1.sort();
-    arr2.sort();
+    userIds.sort();
+    reqParamas.sort();
 
-    if (arr2.length == 1) {
-      if (arr2[0] == arr1[0] && arr2[0] == arr1[1]) return 1;
+    if (userIds.length === 1) {
+      if (userIds[0] === reqParamas[0] && userIds[0] === reqParamas[1]) return 1;
       else return 0;
     } else {
-      if (arr1[0] == arr2[0] && arr1[1] == arr2[1]) return 1;
+      if (reqParamas[0] === userIds[0] && reqParamas[1] === userIds[1]) return 1;
       else return 0;
     }
   });
@@ -85,11 +92,48 @@ app.get("/getChatRoom/:userId/:teamMateId", (req, res) => {
   });
 
   chatRoom.messageIds = messageOfChatRoom;
-  res.send(chatRoom);
+
+  const chatRoomWithMessages = {...chatRoom};
+  chatRoomWithMessages.messages = chatRoomWithMessages.messageIds;
+  delete chatRoomWithMessages.messageIds;
+  res.send(chatRoomWithMessages);
 });
 
+app.put("/addMessage/:chatRoomId", (req, res) => {
+  const file = fs.readFileSync("./database/messages.json");
 
+  //data of post request
+  const data = req.body;
+  data.id = uuidv4();
+  if (file.length === 0) {
+    fs.writeFileSync("./database/messages.json", JSON.stringify([data]));
+  } else {
+    const json = JSON.parse(file.toString());
+    json.push(data);
+    fs.writeFileSync("./database/messages.json", JSON.stringify(json));
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  const chatRoom = fs.readFileSync("./database/chatroom.json", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  let chatRoomData = JSON.parse(chatRoom);
+
+  const currentChatRoom = chatRoomData.find((room) => {
+    return room.id === req.params.chatRoomId;
+  });
+
+  currentChatRoom.messageIds.push(req.body.id);
+
+  chatRoomData = chatRoomData.map((room) => {
+    if (room.id === req.params.chatRoomId) return currentChatRoom;
+    return room;
+  });
+
+  fs.writeFileSync("./database/chatroom.json", JSON.stringify(chatRoomData));
+  res.json(data);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
